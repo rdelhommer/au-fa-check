1928b1446e4d477e97e52877da3edff99e5d6686
Rick Delhommer <rdelhommer@gmail.com>
2019-05-04 10:57:51 -0700

    add focus property. need to do styles

--- src/fa-check/fa-check.ts
+++ src/fa-check/fa-check.ts
@@ -1,9 +1,7 @@
-import { bindable, autoinject, customElement } from "aurelia-framework";
+import { bindable, autoinject, customElement, observable, computedFrom } from "aurelia-framework";
 import { globalConfig } from "./fa-check-config";
 
 // TODO: keyboard control and focus/active styles
-// TODO: Get config working
-// TODO: react/bootstrap theming in config
 // TODO: react styles
 // TODO: checkbox size in config (just normal and large for now
 // TODO: square vs circle container in config
@@ -18,6 +16,17 @@ export class FaCheck {
   @bindable checkIcon: string
   @bindable orientation: 'horizontal' | 'vertical'
   @bindable selectedColor: string
+
+  isInputFocused: boolean
+
+  @computedFrom('disabled', 'checked', 'isInputFocused')
+  get labelClasses() {
+    return [
+      this.disabled ? 'disabled' : undefined,
+      // this.checked ? 'disabled' : undefined,
+      this.isInputFocused ? 'focused' : undefined,
+    ].filter(x => !!x).join(' ')
+  }
 
   toggleChecked(event: Event) {
     if (this.disabled) return;
