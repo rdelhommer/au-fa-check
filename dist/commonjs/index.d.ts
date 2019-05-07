a86e6338a0756b06fc88ceeaad718e0d23bf1157
Rick Delhommer <rdelhommer@gmail.com>
2019-05-07 10:54:21 -0700

    remove dist from git

--- dist/commonjs/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
-import { IFaCheckConfig, FaCheckSize, FaCheckOrientation, FaCheckIconType, FaCheckShape, FaCheckTheme } from './fa-check/fa-check-config';
-export declare function configure(aurelia: FrameworkConfiguration, config: IFaCheckConfig): void;
-export { IFaCheckConfig, FaCheckSize, FaCheckTheme, FaCheckShape, FaCheckIconType, FaCheckOrientation };
--- dist/native-modules/utils/theme-controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { IFaCheckConfig } from "../fa-check/fa-check-config";
-export declare class ThemeController {
-    private getVariableKey;
-    updateVariable(element: HTMLElement, key: string, value: string): void;
-    configureTheme(element: HTMLElement, config?: IFaCheckConfig): void;
-}
-export declare let themeController: ThemeController;
